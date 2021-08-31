6f7b79f21491d01382590af27688d5287e4f7950
Precilla Aranha <precillaaranha16@gmail.com>
2021-08-31 20:42:50 +0530

    BranchA

--- src/Test/A.java
+++ src/Test/A.java
@@ -2,6 +2,8 @@ package Test;
 
 public class A {
 		public static void main(String[] args) {
-			System.out.println("A");
+			System.out.println("A");	
+			System.out.println("Hi");
+			System.out.println("B");
 		}
 }
