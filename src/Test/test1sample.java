e1a535e98d70414f6f023e4db57d2c818aece16a
Precilla Aranha <precillaaranha16@gmail.com>
2021-08-31 20:37:21 +0530

    Preciclla_BranchA

---
+++ src/Test/test1sample.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class test1sample {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Clas");
+		System.out.println("A");
+	}
+
+}
